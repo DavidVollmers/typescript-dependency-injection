d1edb0752fcb0badc6738e8e599160db85fb53a4
David Vollmers <github@david.vollmers.org>
2020-06-16 13:39:31 +0200

    fixes

--- packages/tsdi-vue/lib/vuejs-plugin.ts
+++ packages/tsdi-vue/lib/vuejs-plugin.ts
@@ -1,11 +1,11 @@
-import _Vue                  from 'vue'
-import {DependencyContainer} from '@dvolper/tsdi'
+import _Vue, {PluginFunction} from 'vue'
+import {DependencyContainer}  from '@dvolper/tsdi'
 
 export interface VueJSPluginOptions {
     readonly builder: ( dc: DependencyContainer ) => void
 }
 
-export function VueJSPlugin( Vue: _Vue, options?: VueJSPluginOptions ): void {
+export const VueJSPlugin: PluginFunction<VueJSPluginOptions> = function ( Vue: typeof _Vue, options?: VueJSPluginOptions ): void {
     _Vue.mixin( {
         beforeMount() {
             const dc = new DependencyContainer
